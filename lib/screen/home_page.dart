62724c9016c5a59fea69609b1019000f36ff967c
Aman Nanda <34944818+amannanda8@users.noreply.github.com>
2021-03-10 17:52:53 +0530

    added button

--- lib/screen/home_page.dart
+++ lib/screen/home_page.dart
@@ -10,8 +10,28 @@ class HomePage extends StatelessWidget {
         title: Text('Fake Mail'),
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Image.asset('/img.png'),
+          Image.asset('assets/img.png'),
+          Card(
+            elevation: 10.0,
+            margin: EdgeInsets.all(10),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text('example@gmail.com'),
+            ),
+          ),
+          Row(
+            children: [
+              OutlinedButton.icon(
+                onPressed: () {},
+                icon: Icon(Icons.redo),
+                label: Text('Regenerate'),
+
+                
+              ),
+            ],
+          ),
         ],
       ),
     );
