82a42d492cb83012fc6301d91fa7f0a1c4d79e74
bhavyatyagi <bhavyatyagi16@gmail.com>
2021-03-17 12:18:08 +0530

    added mailboxModel ApiResponse

--- lib/models/mailbox_model.dart
+++ lib/models/mailbox_model.dart
@@ -4,34 +4,61 @@
 
 import 'dart:convert';
 
-List<MailboxModel> mailboxModelFromJson(String str) => List<MailboxModel>.from(json.decode(str).map((x) => MailboxModel.fromJson(x)));
+import 'package:http/http.dart';
+import 'package:temper_mail/services/api_endpoints.dart';
+import 'package:temper_mail/services/api_response.dart';
 
-String mailboxModelToJson(List<MailboxModel> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+// Used app.quicktype.io for generating model class for API response
+
+List<MailboxModel> mailboxModelFromJson(String str) => List<MailboxModel>.from(
+    json.decode(str).map((x) => MailboxModel.fromJson(x)));
+
+String mailboxModelToJson(List<MailboxModel> data) =>
+    json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
 
 class MailboxModel {
-    MailboxModel({
-        this.id,
-        this.from,
-        this.subject,
-        this.date,
-    });
+  MailboxModel({
+    this.id,
+    this.from,
+    this.subject,
+    this.date,
+  });
 
-    int id;
-    String from;
-    String subject;
-    DateTime date;
+  int id;
+  String from;
+  String subject;
+  DateTime date;
 
-    factory MailboxModel.fromJson(Map<String, dynamic> json) => MailboxModel(
+  factory MailboxModel.fromJson(Map<String, dynamic> json) => MailboxModel(
         id: json["id"],
         from: json["from"],
         subject: json["subject"],
         date: DateTime.parse(json["date"]),
-    );
+      );
 
-    Map<String, dynamic> toJson() => {
+  Map<String, dynamic> toJson() => {
         "id": id,
         "from": from,
         "subject": subject,
         "date": date.toIso8601String(),
-    };
+      };
+}
+
+Future<ApiResponse> getMailBox(String username, String domain) async {
+  Response response = await get(Uri.parse(
+      baseURL + '?action=getMessages&login=$username&domain=$domain'));
+
+  ApiResponse res;
+
+  try {
+    if (response.statusCode >= 200 && response.statusCode <= 205) {
+      res = ApiResponse(data: response.body);
+    }
+  } catch (e) {
+    res = ApiResponse(
+      error: true,
+      errorMessage: e.toString(),
+    );
+  }
+  return res;
 }
--- lib/services/api_endpoints.dart
+++ lib/services/api_endpoints.dart
@@ -1,5 +1,3 @@
 const String baseURL = 'https://www.1secmail.com/api/v1/';
 
 const String genEmail = '?action=genRandomMailbox&count=1';
-
-
